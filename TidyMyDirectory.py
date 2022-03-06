5fb26bdbdd110bba7803eccb68b99c72630823ae
Melissa <melissalokvw+gitkraken@gmail.com>
2022-03-06 17:11:13 +1300

    create frontend, add logo and icon, edit window title and icon

--- TidyMyDirectory.py
+++ TidyMyDirectory.py
@@ -1,12 +1,11 @@
 import os
 import sys
 
-from PySide2 import QtGui
+from PySide2 import QtGui, QtCore
+from PySide2.QtGui import QPixmap, QCursor, QIcon
 from PySide2.QtWidgets import (
-    QDesktopWidget,
     QApplication,
     QLabel,
-    QToolTip,
     QPushButton,
     QMessageBox,
     QFileDialog,
@@ -15,7 +14,7 @@ from PySide2.QtWidgets import (
     QMainWindow,
     QDialogButtonBox,
     QVBoxLayout,
-    QListWidgetItem
+    QListWidgetItem, QGridLayout
 )
 
 
@@ -32,16 +31,15 @@ class CustomDialog(QDialog):
         self.buttonBox.rejected.connect(self.reject)
 
         path = selected_directory()
-
-        list_of_files = QListWidget()
-
         files = [f for f in os.listdir(path)]
+        list_of_files = QListWidget()
 
         for count, f in enumerate(files):
             QListWidgetItem(f, list_of_files)
 
         self.layout = QVBoxLayout()
-        folder = QLabel("Chosen Directory: " + path)
+
+        folder = QLabel("Chosen Directory: <strong>" + path + "</strong>")
         message = QLabel("\nThese folders will be renamed sequentially by extension, is that okay?")
 
         self.layout.addWidget(folder)
@@ -78,6 +76,7 @@ def rename_resequence():
 
     files = sort_by_ext(path)
 
+    count = 0
     prod_png_count = 0
     prod_jpg_count = 0
     weta_png_count = 0
@@ -117,6 +116,20 @@ def rename_resequence():
             print(f"Renaming " + f + " to " + new_name)
             os.rename(f, new_name)
 
+        #  todo: For all other names and extensions
+        else:
+            if f_ext == ".png":
+                prod_png_count += 1
+                f_name = renamed_file(f_name, prod_png_count)
+
+            elif f_ext == ".jpg":
+                prod_jpg_count += 1
+                f_name = renamed_file(f_name, prod_jpg_count)
+
+            new_name = f'{f_name}{f_ext}'
+            print(f"Renaming " + f + " to " + new_name)
+            os.rename(f, new_name)
+
 
 class TidyMyDirectory(QMainWindow):
 
@@ -126,16 +139,35 @@ class TidyMyDirectory(QMainWindow):
         self.setup()
 
     def setup(self):
+        self.setFixedSize(900, 600)
+        self.setStyleSheet("background: 'white';")
+        self.setWindowTitle('Tidy my Directory!')
+        self.setWindowIcon(QIcon("Icon.png"))
 
-        browse_button = QPushButton("Browse folders", self)
-        browse_button.resize(browse_button.sizeHint())
-        browse_button.move(400, 300)
+        # Setting the logo and icon of the program
+        logo = QLabel(self)
+        image = QPixmap('Logo.png')
+        logo.setPixmap(image)
+        logo.resize(logo.sizeHint())
+        logo.move(183, 110)
+
+        # Button UI
+        browse_button = QPushButton("Choose a folder to tidy up", self)
+        browse_button.move(80, 230)
+        browse_button.resize(750, 270)
+        browse_button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+        browse_button.setStyleSheet(
+            "*{ background-color: '#36B0CB';" +
+            "border-radius: 35px;" +
+            "font-size: 20px;" +
+            "font-weight: 2px;" +
+            "color: 'white';" +
+            "padding: 10px 0px;" +
+            "margin: 100px 200px; }" +
+            "*:hover{background: '#36A2BB'; }"
+        )
         browse_button.clicked.connect(self.button_clicked)
 
-        self.setFixedSize(900, 600)
-        self.center_window()
-
-        self.setWindowTitle('Tidy my Directory!')
         self.show()
 
     def closeEvent(self, event: QtGui.QCloseEvent):
@@ -151,23 +183,19 @@ class TidyMyDirectory(QMainWindow):
         dlg = CustomDialog(self)
         button = dlg.exec_()
 
-        if button == 1: # 1 = QDialogBox.Ok
+        if button == 1:  # 1 = QDialogBox.Ok
             rename_resequence()
             print("success")
         else:
             print("cancelled")
 
-    def center_window(self):
-
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
 
 def main():
     app = QApplication(sys.argv)
-    ex = TidyMyDirectory()
+
+    GUI = TidyMyDirectory()
+    GUI.show()
+
     sys.exit(app.exec_())
 
 
